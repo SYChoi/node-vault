f88ae3c67efc270e33c6f85e3d16bca38faa3aa8
sychoi <sychoi@standby.kr>
2023-08-07 15:26:47 +0900

    changed request to postman-request

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
-import * as request from "request";
+import * as request from "postman-request";
 
 declare namespace NodeVault {
     interface Option {
